c16d1a80647c54e308f0679f00548aeabe7c53f1
ion <ionix@protonmail.com>
2017-08-06 16:56:25 +0200

    word smash not working :/

--- word_smash/word.asm
+++ word_smash/word.asm
@@ -1,26 +1,38 @@
-BasicUpstart2(start)
+BasicUpstart2(preload)
 
-start:
-    jsr $e544
+.var screen = $0400
+.var shift = $00fb
+
+preload:
+    lda #147
+    jsr $ffd2 // jump to text output
     lda #$00
     sta $d020
     sta $d021
+    lda #0
+    sta shift
 
-    ldx #$00 
+    jmp start
+
+start:
+    ldx #5
+    ldy shift
     jsr write
-
-    jmp *
-  
+    jmp start
+    
 write:
-    lda msg, x 
-    sta $0400,x 
-    lda #$01 
-    sta $d800,x 
-    inx 
-    cpx #52
+    lda msg, x
+    sta screen, y
+    dey
+    dex
+    cpx #-1
     bne write
+    lda msg+2 // get space
+    sta screen, y // overwrite first char
+    ldy #0
+    inc shift
     rts
 
 msg:
-    .text "lorem ipsum forgot ipserum me not know lorem anymore "
-    .byte $00  
+    .text "by ion"
+    .byte $00
